7a221fd40e1aed6cf1ee1a6d62c1ae01e9069492
Francis Kingori <Francis.Kingori@tovutigroup.com>
2021-03-14 19:20:06 +0300

    update

---
+++ DBL/Entities/Companydetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace DBL.Entities
+{
+    [Table("Companydetails")]
+    public class Companydetails
+    {
+        [NotMapped]
+        public static string TableName { get { return "Companydetails"; } }
+
+        public long Companycode { get; set; }
+        public string Companyname { get; set; }
+        public string companyaddress { get; set; }
+        public long country { get; set; }
+        public string City { get; set; }
+        public long Currency { get; set; }
+        public bool Isactive { get; set; }
+        public DateTime Datecreated { get; set; }
+        public DateTime Datemodified { get; set; }
+        public string Createdby { get; set; }
+        public string Modifiedby { get; set; }
+    }
+}
