84fe093a36fc6f0127279f29e33065955247f477
Francis Kingori <Francis.Kingori@tovutigroup.com>
2021-07-16 07:09:54 +0300

    update

---
+++ Fumasi/Controllers/PricesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fumasi.Controllers
+{
+    public class PricesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
